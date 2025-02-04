8d8f9332870371b1d7c3858e7f5dd9035bed89f7
lesedi thibile <lesedi.xys11@gmail.com>
2025-02-05 00:42:38 +0200

    Refactor the code in the TaskCLI

--- cli/TaskCli.java
+++ cli/TaskCli.java
@@ -16,17 +16,8 @@ public class TaskCli {
     }
 
     public void displayCli() {
-        String mainMenu = """
-        
-        1. Add New Task
-        2. List All Tasks
-        3. Mark Task As Completed
-        4. Remove Task
-        5. Exit Program
-        """;
-
         while (true) {
-            System.out.println(mainMenu);
+            printMenu();
 
             int choice;
             try {
@@ -36,96 +27,114 @@ public class TaskCli {
             }
 
             switch (choice) {
-                case 1 -> {
-                    System.out.println("Add New Task");
-                    var desc = getValue("Description:");
-
-                    LocalDate validDate;
-
-                    try {
-                        var year = getInt("year: ");
-                        var month = getInt("month: ");
-                        var day = getInt("day: ");
-
-                        if(year < 2025) {
-                            throw new DateTimeException("Year cannot be less than 2025");
-                        }
-
-                        validDate = LocalDate.of(year, month, day);
-                    } catch (InputMismatchException e) {
-                        System.out.println("Error: Enter a valid input");
-                        break;
-                    } catch (DateTimeException e) {
-                        System.out.println("Error: enter a valid date");
-                        break;
-                    }
-
-                    var task = taskManager.addTask(desc, validDate);
-
-                    if (task) {
-                        System.out.println("Task added successfully");
-                    } else {
-                        System.out.println("Something went wrong. Please try again!");
-                    }
-                }
-                case 2 -> {
-                    var tasks = taskManager.getTasks();
-
-                    if(tasks.isEmpty()) {
-                        System.out.println("You currently have no tasks");
-                        break;
-                    }
-
-                    var allTasks = tasks.size();
-                    var completedTasks = tasks.stream().filter(Task::isCompleted).toList().size();
-
-                    System.out.println("All Tasks: " + "[" + allTasks + "]");
-                    System.out.println("Completed Tasks: " + "[" + completedTasks + "]");
-                    System.out.printf("| %-10s | %-20s | %-10s | %-10s | \n", "Task ID", "Description", "Due Date", "Completed");
-
-                    tasks.forEach(task ->
-                        System.out.printf(
-                            "| %-10s | %-20s | %-10s | %-10s | \n",
-                            task.getId(),
-                            task.getDescription(),
-                            task.getDueDate(),
-                            task.isCompleted()
-                        )
-                    );
-
-                }
-                case 3 -> {
-                    var taskId = getValue("Enter Task ID:");
-                    var exists = taskManager.contains(taskId);
-
-                    if (!exists) {
-                        System.out.println("task not found");
-                        break;
-                    }
-
-                    taskManager.markCompleted(taskId);
-                    System.out.println("Task Marked As Completed");
-                }
-                case 4 -> {
-                    var taskId = getValue("Enter Task ID:");
-                    var exists = taskManager.contains(taskId);
-
-                    if (!exists) {
-                        System.out.println("task not found");
-                        break;
-                    }
-
-                    taskManager.deleteTask(taskId);
-                    System.out.println("Task Removed Successfully");
-                }
+                case 1 -> addNewTask();
+                case 2 -> viewAllTasks();
+                case 3 -> markTaskAsCompleted();
+                case 4 -> deleteTask();
                 case 5 -> {
                     System.out.println("""
                     Thanks for using our program <3
                     """);
-                    return;
+                    break;
                 }
             }
         }
+    }
+
+    public void addNewTask() {
+        System.out.println("Add New Task");
+        var desc = getValue("Description:");
+
+        LocalDate validDate = null;
+
+        try {
+            var year = getInt("year: ");
+            var month = getInt("month: ");
+            var day = getInt("day: ");
+
+            if(year < 2025) {
+                throw new DateTimeException("Year cannot be less than 2025");
+            }
+
+            validDate = LocalDate.of(year, month, day);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Enter a valid input");
+        } catch (DateTimeException e) {
+            System.out.println("Error: enter a valid date");
+        }
+
+        var task = taskManager.addTask(desc, validDate);
+
+        if (task) {
+            System.out.println("Task added successfully");
+        } else {
+            System.out.println("Something went wrong. Please try again!");
+        }
+    }
+
+    public void viewAllTasks() {
+        var tasks = taskManager.getTasks();
+
+        if(tasks.isEmpty()) {
+            System.out.println("You currently have no tasks");
+            return;
+        }
+
+        var allTasks = tasks.size();
+        var completedTasks = tasks.stream().filter(Task::isCompleted).toList().size();
+
+        System.out.println("All Tasks: " + "[" + allTasks + "]");
+        System.out.println("Completed Tasks: " + "[" + completedTasks + "]");
+        System.out.printf("| %-10s | %-20s | %-10s | %-10s | \n", "Task ID", "Description", "Due Date", "Completed");
+
+        tasks.forEach(task ->
+                System.out.printf(
+                        "| %-10s | %-20s | %-10s | %-10s | \n",
+                        task.getId(),
+                        task.getDescription(),
+                        task.getDueDate(),
+                        task.isCompleted()
+                )
+        );
+    }
+
+    public void markTaskAsCompleted() {
+        var taskId = getValue("Enter Task ID of a Task you want to mark as completed:");
+        var exists = taskManager.contains(taskId);
+
+        if (!exists) {
+            System.out.println("task not found");
+            return;
+        }
+
+        taskManager.markCompleted(taskId);
+        System.out.println("Task Marked As Completed");
+    }
+
+    public void deleteTask() {
+        var taskId = getValue("Enter Task ID:");
+        var exists = taskManager.contains(taskId);
+
+        if (!exists) {
+            System.out.println("task not found");
+            return;
+        }
+
+        taskManager.deleteTask(taskId);
+        System.out.println("Task Removed Successfully");
+    }
+
+    private void printMenu() {
+        String mainMenu = """
+        
+        1. Add New Task
+        2. List All Tasks
+        3. Mark Task As Completed
+        4. Remove Task
+        5. Exit Program
+        """;
+
+        System.out.println(mainMenu);
     }
 
     public String getValue(String prompt) {
